b4c1af4021d28dceb38887495dee9d43c56a0ca1
Leandro Rodrigues <oleandroprodrigues@gmail.com>
2026-04-17 12:24:04 -0300

    test(infrastructure): test event publisher

---
+++ EventDrivenArchitecture.RabbitMQ.Infrastructure.UnitTests/Messaging/InMemory/InMemoryEventPublisherTests.cs
@@ -0,0 +1,26 @@
+﻿using EventDrivenArchitecture.RabbitMQ.Domain.Events;
+using EventDrivenArchitecture.RabbitMQ.Infrastructure.Messaging.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EventDrivenArchitecture.RabbitMQ.Infrastructure.UnitTests.Messaging.InMemory;
+
+public sealed class InMemoryEventPublisherTests
+{
+    [Fact]
+    public async Task PublishAsync_ShouldStoreEvent()
+    {
+        var publisher = new InMemoryEventPublisher();
+
+        var evt = new OrderCreated("1", "2", 10, DateTime.UtcNow);
+
+        await publisher.PublishAsync(evt);
+
+        Assert.Single(publisher.PublishedEvents);
+    }
+}
+
